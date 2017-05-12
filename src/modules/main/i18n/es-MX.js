f139efc00127e03810a5a8174fc0f18c0beeecf2
Guillermo <gcruz@anzen.com.mx>
2017-05-12 17:26:13 -0500

    Estilos en la directiva navBar vertical

--- src/modules/main/i18n/es-MX.js
+++ src/modules/main/i18n/es-MX.js
@@ -1,6 +1,11 @@
 module.exports = {
     language: 'es-MX',
     translate: {
-        TITLE_HOME: 'Página principal de acceso'
+        TITLE_HOME: 'Página principal de acceso',
+        USERS: 'Usuarios',
+        APPLICATIONS: 'Aplicaciones',
+        PROVIDERS: 'Proveedores',
+        USERS_CITI: 'Citi Usuarios',
+        CATALOGS: 'Catálogos'
     }
 };
