cb9b1d30f0e3a2ec3d5e2889155ecc61a0eb3f4f
Guillermo <gcruz@anzen.com.mx>
2017-06-30 11:39:13 -0500

    Add flow to show information of citi users

--- src/modules/theCitiUsers/i18n/en-US.js
+++ src/modules/theCitiUsers/i18n/en-US.js
@@ -4,6 +4,11 @@ module.exports = {
         TITLE_CITI_USERS: 'Citi Resource',
         CITI_USERS_DOMAINS: 'Domains',
         CITI_USERS_PER_DOMAIN: 'Staff per Domain',
-        CITI_USERS_SPECIFIC_DETAIL: 'Specific detail Citi staff'
+        CITI_USERS_SPECIFIC_DETAIL: 'Specific detail Citi staff',
+
+        /* Label Tables */
+        NAME_DOMAIN_CITI_U: 'Name',
+        JOB_CITI_U: 'Job',
+        NAME_CITI_U: 'Name'
     }
 };
--- src/modules/theCitiUsers/i18n/es-MX.js
+++ src/modules/theCitiUsers/i18n/es-MX.js
@@ -4,6 +4,16 @@ module.exports = {
         TITLE_CITI_USERS: 'Recursos Citi',
         CITI_USERS_DOMAINS: 'Dominios',
         CITI_USERS_PER_DOMAIN: 'Personal por Dominio',
-        CITI_USERS_SPECIFIC_DETAIL: 'Detalle específico Personal Citi'
+        CITI_USERS_SPECIFIC_DETAIL: 'Detalle específico Personal Citi',
+
+        /* Label Tables */
+        NAME_DOMAIN_CITI_U: 'Nombre',
+        JOB_CITI_U: 'Puesto',
+        NAME_CITI_U: 'Nombre',
+
+        /* Labels Details User */
+        DOMAIN_CITI_U: 'Dominio',
+        JOB_CITI_U: 'Puesto',
+        NAME_CITI_U: 'Nombre'
     }
 };
