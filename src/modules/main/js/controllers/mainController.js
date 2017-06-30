46f5296bc331f719f6a8913f32e7704d9c5d1134
Guillermo <gcruz@anzen.com.mx>
2017-06-30 11:35:43 -0500

    Add flag to hide menu options per profile

--- src/modules/main/js/controllers/mainController.js
+++ src/modules/main/js/controllers/mainController.js
@@ -9,14 +9,20 @@ var angular = require('angular');
  * @returns {undefinided} This function does not return values.
  */
 
-var mainCtrl = function($scope, $location) {
+var mainCtrl = function($scope, $location,$cookies, $window) {
 
     var $this = this;
+
+    $this.adm =  $cookies.get("adm") == "true" ? true : false;
+    $this.app = $cookies.get("app") == "true" ? true : false;
+    $this.provs = $cookies.get("provs") == "true" ? true : false;
+    $this.citiU =$cookies.get("citiU") == "true" ? true : false;
+    $this.cat = $cookies.get("cat") == "true" ? true : false;
+    $this.showButtons = $cookies.get("showButtons") == "true" ? true : false;
 
     $this.navbarMenuView = function(page){
         $location.path("/"+page);
     }
-   
 };
 
 module.exports = angular.module("app.main").controller('mainCtrl', mainCtrl);
